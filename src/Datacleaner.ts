cad473a013ee3f08bb1ad2bc28b86b0c59725b80
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2025-03-18 13:29:27 +0100

    First thoughts on this code

--- src/Datacleaner.ts
+++ src/Datacleaner.ts
@@ -1,19 +1,24 @@
-import {readFileSync, writeFileSync} from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 
 export const clean_Data = (filename: any) => {
-  let data = readFileSync('./data/input/test_data.js', 'utf-8');
+    /** input param not used anywhere? why? */
 
-  const parsed_date = JSON.parse(data);
+    let data = readFileSync('./data/input/test_data.js', 'utf-8'); /** is there a reason of hardcoding the path in production code? */
 
-    let mappedData = parsed_date.map((i: any) => {
-      return {
-      user_id: i.user_id,
-      organisation: i.organisation_id,
-      data: i.steps_data
-      }  
+    const parsed_date = JSON.parse(data); /** how do we know JSON is successfully parsed/valid? no checks here */
+
+    let mappedData = parsed_date.map((i: any) => { /** what if the JSON contains some other structures? no checks here */
+        return {
+            user_id: i.user_id,
+            organisation: i.organisation_id,
+            data: i.steps_data
+        }
     });
 
-  let result = writeFileSync('./data/output/test_data.js', JSON.stringify(mappedData), 'utf-8')
+    let result = writeFileSync('./data/output/test_data.js', JSON.stringify(mappedData), 'utf-8');
+    /** according to the above:
+     * 1. is there a reason of hardcofing the path in production code?
+     * 2. what do we need this for? declared and not used*/
 
-  return true;
+    return true; /** and why are we returning true no matter what? */
 }
--- tests/map_data.test.ts
+++ tests/map_data.test.ts
@@ -1,9 +1,11 @@
-import {clean_Data} from '../src/Datacleaner';
+import { clean_Data } from '../src/Datacleaner'; /** this layout is not generic, we shall import from 'packagename' */
 
 describe("The test", () => {
-  it("Should pass", () => {
-    let result = clean_Data('test_data.js');
+    it("Should pass", () => {
+        let result = clean_Data('test_data.js'); /** clean_Data will NOT: 1. use this file, 2. return real result (but always true) */
 
-    expect(result).toBe(true)
-  });
+        /** 3. if clean_Data will be fixed, you shall ensure that test_data.js can be read (specify full path here) */
+
+        expect(result).toBe(true) /** always true, according to source code */
+    });
 })
